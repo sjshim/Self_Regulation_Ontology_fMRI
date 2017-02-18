ae2bd55774d20232c32c7650dd53744782461a1f
ian <ianeisenberg90@gmail.com>
2017-02-17 17:36:12 -0800

    continued working on calculating fmri dvs

--- data_preparation/process_data.py
+++ data_preparation/process_data.py
@@ -1,7 +1,22 @@
-from expanalysis.experiments.processing import clean_data
+from expanalysis.experiments.processing import clean_data, calc_exp_DVs, organize_DVs
 from glob import glob
 import os
 import pandas as pd
+# some DVs are defined in utils if they deviate from normal expanalysis
+from utils import calc_ANT_DV
+
+# set up dictionary of dataframes for each task
+tasks = ['attention_network_task','columbia_card_task_fmri',
+         'discount_fixed', 'dot_pattern_expectancy', 
+         'motor_selective_stop_signal', 'stop_signal', 'stroop', 'survey_medley',
+         'twobytwo', 'ward_and_allport']
+task_dfs = {}
+for task in tasks:
+    task_path = '../Data/processed/group_data/%s.csv' % task
+    if os.path.exists(task_path):
+        task_dfs[task] = pd.read_csv(task_path)
+    else:
+        task_dfs[task] = pd.DataFrame()
 
 for subj_file in glob('../Data/raw/*/*'):
     filey = os.path.basename(subj_file)
@@ -10,24 +25,40 @@ for subj_file in glob('../Data/raw/*/*'):
     # if this file has already been cleaned, continue
     if os.path.exists(cleaned_file_path):
         continue
-    # else proceed
-    df = pd.read_csv(subj_file)
-    # set time_elapsed in reference to the last trigger of internal calibration
-    start_time = df.query('trial_id == "fmri_trigger_wait"').iloc[-1]['time_elapsed']
-    df.time_elapsed-=start_time
-    # add exp_id to every row
-    exp_id = df.iloc[-2].exp_id
-    if exp_id == 'columbia_card_task_hot':
-        exp_id = 'columbia_card_task_fmri'
-    df.loc[:,'experiment_exp_id'] = exp_id
-    # change column from subject to worker_id
-    df.rename(columns={'subject':'worker_id'}, inplace=True)
-    # post process data, drop rows, etc...
-    df = clean_data(df, exp_id=exp_id)
-    # drop unnecessary rows 
-    drop_dict = {'trial_type': ['text'], 
-                 'trial_id': ['fmri_response_test', 'fmri_scanner_wait', 
-                              'fmri_trigger_wait', 'fmri_buffer']}
-    for row, vals in drop_dict.items():
-        df = df.query('%s not in  %s' % (row, vals))
-    df.to_csv(cleaned_file_path)
+    else:
+        # else proceed
+        df = pd.read_csv(subj_file)
+        # set time_elapsed in reference to the last trigger of internal calibration
+        start_time = df.query('trial_id == "fmri_trigger_wait"').iloc[-1]['time_elapsed']
+        df.time_elapsed-=start_time
+        # add exp_id to every row
+        exp_id = df.iloc[-2].exp_id
+        if exp_id == 'columbia_card_task_hot':
+            exp_id = 'columbia_card_task_fmri'
+        df.loc[:,'experiment_exp_id'] = exp_id
+        # change column from subject to worker_id
+        df.rename(columns={'subject':'worker_id'}, inplace=True)
+        # post process data, drop rows, etc.....
+        df = clean_data(df, exp_id=exp_id)
+        # drop unnecessary rows 
+        drop_dict = {'trial_type': ['text'], 
+                     'trial_id': ['fmri_response_test', 'fmri_scanner_wait', 
+                                  'fmri_trigger_wait', 'fmri_buffer']}
+        for row, vals in drop_dict.items():
+            df = df.query('%s not in  %s' % (row, vals))
+        df.to_csv(cleaned_file_path)
+        task_dfs[exp_id] = pd.concat([task_dfs[exp_id], df], axis=0)
+# save group behavior
+for task,df in task_dfs.items():
+    df.to_csv('../Data/processed/group_data/%s.csv' % task)
+
+# calculate DVs
+for task_data in glob('../Data/processed/group_data/two*'):
+    df = pd.read_csv(task_data)
+    exp_id = df.experiment_exp_id.unique()[0]
+    print(exp_id)
+    if exp_id in ['attention_network_task']:
+        dvs,description = calc_ANT_DV(df, use_group_fun = False)
+        DVs, valence = organize_DVs(dvs)
+    else:
+        DVs, valence, description = calc_exp_DVs(df, use_group_fun = False)
---
+++ data_preparation/utils.py
@@ -0,0 +1,96 @@
+from expanalysis.experiments.jspsych_processing import ANT_HDDM, EZ_diffusion, \
+    get_post_error_slow, group_decorate
+
+@group_decorate(group_fun = ANT_HDDM)
+def calc_ANT_DV(df, dvs = {}):
+    """ Calculate dv for attention network task: Accuracy and average reaction time
+    :return dv: dictionary of dependent variables
+    :return description: descriptor of DVs
+    """
+    # add columns for congruency sequence effect
+    df.insert(0,'flanker_shift', df.flanker_type.shift(1))
+    df.insert(0, 'correct_shift', df.correct.shift(1))
+    
+    # post error slowing
+    post_error_slowing = get_post_error_slow(df.query('exp_stage == "test"'))
+    
+    # subset df
+    missed_percent = (df['rt']==-1).mean()
+    df = df.query('rt != -1').reset_index(drop = True)
+    df_correct = df.query('correct == True')
+    
+    # Get DDM parameters
+    dvs.update(EZ_diffusion(df, condition = 'flanker_type'))
+    # get DDM across all trials
+    ez_alltrials = EZ_diffusion(df)
+    dvs.update(ez_alltrials)
+    
+    # Calculate basic statistics - accuracy, RT and error RT
+    dvs['acc'] = {'value':  df.correct.mean(), 'valence': 'Pos'}
+    dvs['avg_rt_error'] = {'value':  df.query('correct == False').rt.median(), 'valence': 'NA'}
+    dvs['std_rt_error'] = {'value':  df.query('correct == False').rt.std(), 'valence': 'NA'}
+    dvs['avg_rt'] = {'value':  df_correct.rt.median(), 'valence': 'Neg'}
+    dvs['std_rt'] = {'value':  df_correct.rt.std(), 'valence': 'NA'}
+    dvs['missed_percent'] = {'value':  missed_percent, 'valence': 'Neg'}
+    dvs['post_error_slowing'] = {'value':  post_error_slowing, 'valence': 'Pos'}
+    
+    # Get three network effects
+    cue_rt = df_correct.groupby('cue').rt.median()
+    flanker_rt = df_correct.groupby('flanker_type').rt.median()
+    cue_acc = df.groupby('cue').correct.mean()
+    flanker_acc = df.groupby('flanker_type').correct.mean()
+    
+    dvs['orienting_rt'] = {'value':  (cue_rt.loc['double'] - cue_rt.loc['spatial']), 'valence': 'Pos'}
+    dvs['conflict_rt'] = {'value':  (flanker_rt.loc['incongruent'] - flanker_rt.loc['congruent']), 'valence': 'Neg'}
+    dvs['orienting_acc'] = {'value':  (cue_acc.loc['double'] - cue_acc.loc['spatial']), 'valence': 'NA'}
+    dvs['conflict_acc'] = {'value':  (flanker_acc.loc['incongruent'] - flanker_acc.loc['congruent']), 'valence': 'Pos'}
+    
+    # DDM equivalents
+    param_valence = {'drift': 'Pos', 'thresh': 'Pos', 'non_decision': 'NA'}
+    if set(['EZ_drift_congruent', 'EZ_drift_incongruent']) <= set(dvs.keys()):
+        cue_ddm = EZ_diffusion(df,'cue')
+        for param in ['drift','thresh','non_decision']:
+            dvs['orienting_EZ_' + param] = {'value':  cue_ddm['EZ_' + param + '_double']['value'] - cue_ddm['EZ_' + param + '_spatial']['value'], 'valence': param_valence[param]}
+            dvs['conflict_EZ_' + param] = {'value':  dvs['EZ_' + param + '_incongruent']['value'] - dvs['EZ_' + param + '_congruent']['value'], 'valence': param_valence[param]}
+
+    for param in ['drift','thresh','non_decision']:
+        if set(['hddm_' + param + '_congruent', 'hddm_' + param + '_incongruent']) <= set(dvs.keys()):
+            dvs['conflict_hddm_' + param] = {'value':  dvs['hddm_' + param + '_incongruent']['value'] - dvs['hddm_' + param + '_congruent']['value'], 'valence': param_valence[param]}
+        if set(['hddm_' + param + '_double', 'hddm_' + param + '_spatial']) <= set(dvs.keys()):
+            dvs['orienting_hddm_' + param] = {'value':  dvs['hddm_' + param + '_double']['value'] - dvs['hddm_' + param + '_spatial']['value'], 'valence': param_valence[param]}
+    # remove unnecessary cue dvs
+    for key in list(dvs.keys()):
+        if any(x in key for x in ['double', 'spatial']):
+            del dvs[key]
+            
+    #congruency sequence effect
+    congruency_seq_rt = df_correct.query('correct_shift == True').groupby(['flanker_shift','flanker_type']).rt.median()
+    congruency_seq_acc = df.query('correct_shift == True').groupby(['flanker_shift','flanker_type']).correct.mean()
+    
+    try:
+        seq_rt = (congruency_seq_rt['congruent','incongruent'] - congruency_seq_rt['congruent','congruent']) - \
+            (congruency_seq_rt['incongruent','incongruent'] - congruency_seq_rt['incongruent','congruent'])
+        seq_acc = (congruency_seq_acc['congruent','incongruent'] - congruency_seq_acc['congruent','congruent']) - \
+            (congruency_seq_acc['incongruent','incongruent'] - congruency_seq_acc['incongruent','congruent'])
+        dvs['congruency_seq_rt'] = {'value':  seq_rt, 'valence': 'NA'}
+        dvs['congruency_seq_acc'] = {'value':  seq_acc, 'valence': 'NA'}
+    except KeyError:
+        pass
+    
+    description = """
+    DVs for "alerting", "orienting" and "conflict" attention networks are of primary
+    interest for the ANT task, all concerning differences in RT. 
+    Alerting is defined as nocue - double cue trials. Positive values
+    indicate the benefit of an alerting double cue. Orienting is defined as center - spatial cue trials.
+    Positive values indicate the benefit of a spatial cue. Conflict is defined as
+    incongruent - congruent flanker trials. Positive values indicate the benefit of
+    congruent trials (or the cost of incongruent trials). RT measured in ms and median
+    RT are used for all comparisons.
+    
+    DDM comparisons are used for conflict conditions. Drift is expected to be lower for the more difficult
+    incongruent condition (incongruent-congruent is negative). Thus higher values are good, meaning there
+    is less of a disparity. Thresholds may be higher in conflict conditions. Higher values (incongruent-congruent)
+    are also good, as they indicate adapatible changes in the amount of evidence needed based on the 
+    difficulty of the trial.
+    """
+    return dvs, description
