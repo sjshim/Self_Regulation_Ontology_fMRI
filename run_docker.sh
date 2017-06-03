a2b45b61efeb5b1704c101611cea04cb725e914e
ian <ianeisenberg90@gmail.com>
2017-06-02 20:30:32 -0700

    got docker working

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,1 @@
-docker run -ti --rm -p 8888:8888 -v `pwd`/fmri_analysis/Data:/Data:ro  nipype_image 
+docker run -ti --rm -p 8888:8888 -v `pwd`/fmri_analysis/Data:/Data:ro  nipype_image --participant_label $1
