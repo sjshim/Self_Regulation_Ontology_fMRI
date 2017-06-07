4ea896d1938cf12ff9bafa67b319e570e9406772
ianeisenberg90@gmail.com <ieisenbe@sherlock-ln02.stanford.edu>
2017-06-06 17:30:33 -0700

    moved batch files

--- fmri_analysis/run_singularity.sh
+++ fmri_analysis/run_singularity.sh
@@ -1,2 +1,3 @@
-singularity run -B `pwd`/output:/scratch -B `pwd`/Data:/Data singularity_images/nipype_image-2017-06-06-d2bbd7987fd2.img --participant_label $1 
+#singularity run -B `pwd`/output:/scratch -B `pwd`/Data:/Data singularity_images/nipype_image-2017-06-06-d2bbd7987fd2.img --participant_label $1 
 
+singularity run -B /home/ieisenbe/Self_Regulation_Ontology/fmri_analysis/output:/scratch -B /scratch/PI/russpold/work/ieisenbe/uh2/fmriprep/fmriprep/:/Data singularity_images/nipype_image-2017-06-06-d2bbd7987fd2.img --participant_label s358
