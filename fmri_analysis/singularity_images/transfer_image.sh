e515eb3f92996ad7e403dab7c383e14ba4c05157
ian <ianeisenberg90@gmail.com>
2017-06-12 10:33:21 -0700

    update to function

--- fmri_analysis/singularity_images/transfer_image.sh
+++ fmri_analysis/singularity_images/transfer_image.sh
@@ -1,3 +1,4 @@
 sshfs sherlock:/scratch/PI/russpold/work/ieisenbe/uh2 /mnt/Sherlock_Scratch
+rm -f /mnt/Sherlock_Scratch/singularity_images/*img
 cp *img /mnt/Sherlock_Scratch/singularity_images/
 sudo umount /mnt/Sherlock_Scratch
