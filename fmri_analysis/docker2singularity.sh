7a2d2aee28e8b18cfd97bb8137f6e70aec1b4331
ian <ianeisenberg90@gmail.com>
2017-06-12 10:33:50 -0700

    improved function to take care of the entire transfer of singularity images

--- fmri_analysis/docker2singularity.sh
+++ fmri_analysis/docker2singularity.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# init
-
+rm -f singularity_images/*img
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v /home/ian/Experiments/expfactory/Self_Regulation_Ontology_fMRI/fmri_analysis/singularity_images:/output --privileged -t --rm singularityware/docker2singularity nipype_image
+cd singularity_images
+bash transfer_image.sh
