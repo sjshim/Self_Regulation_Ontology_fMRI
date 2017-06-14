c9b1e8895f63b557eb97636eaa6265026f153bdd
Ian <IanEisenberg@gmail.com>
2017-06-13 18:26:59 -0700

    updated batch scripts

--- fmri_analysis/batch_files/run_task_analysis.sh
+++ fmri_analysis/batch_files/run_task_analysis.sh
@@ -1,6 +1,10 @@
-for path in /scratch/PI/russpold/work/ieisenbe/uh2/fmriprep/fmriprep/sub-s1??
+for path in /scratch/PI/russpold/work/ieisenbe/uh2/fmriprep/fmriprep/sub-s???
 do
     sid=${path: -4}
-    sed "s/{sid}/$sid/g" task_analysis.batch | sbatch -p russpold
+    if [ -f /scratch/PI/russpold/work/ieisenbe/uh2/output/datasink/1stLevel/${sid}_task_DPX/ ]; then
+        echo task analysis already run on $sid DPX, skipping all tasks
+    else
+    	sed "s/{sid}/$sid/g" task_analysis.batch | sbatch -p russpold
+    fi
 done
 
