ce6203b0a02f294a53df5ed282beea16c00b9671
Ian <ianeisenberg90@gmail.com>
2018-01-12 00:31:43 -0800

    additions in design inspection file

--- fmri_analysis/repo_utils/inspect_design.py
+++ fmri_analysis/repo_utils/inspect_design.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 10 12:29:03 2018
-
-@author: ian
-"""
-
 from glob import glob
 from os.path import join
 import pandas as pd
@@ -15,16 +9,31 @@ from utils.display_utils import get_design_df, plot_design
 data_dir = '/mnt/OAK/fmri_analysis/output/1stLevel/'
 task = 'ANT'
 
-paths = glob(join(data_dir, '*%s*' % task))
-path = paths[0]
+# get design/events files that have been output from nipype
+subj_index = 0
+paths = sorted(glob(join(data_dir, '*%s*' % task)))
+path = paths[subj_index]
 df = get_design_df(path)
 
-plot_design(df)
-contrast_paths = glob(join(data_dir, '*%s*' % task, 'contrasts.pkl'))
-contrast_path = contrast_paths[0]
+contrast_paths = sorted(glob(join(data_dir, '*%s*' % task, 'contrasts.pkl')))
+contrast_path = contrast_paths[subj_index]
 contrasts = pickle.load(open(contrast_path, 'rb'))
 
-events_dir = '/mnt/OAK/fmri_analysis/fmriprep/fmriprep'
-events_dirs = glob(join(events_dir, '*','*ses*','func','*%s*' % task))
-event_path = event_paths[0]
+# get events_file
+event_paths = sorted(glob(join('../../Data/event_files','*')))
+event_path = event_paths[subj_index]
 events_df = pd.DataFrame.from_csv(event_path, sep='\t')
+
+# get individual contrast files that have been output from nipype
+
+from nilearn.image import concat_imgs, mean_img, threshold_img
+from nilearn.plotting import plot_glass_brain, plot_stat_map
+
+for contrast_num in range(1, len(contrasts)+1):
+    contrast_name = contrasts[contrast_num-1][0]
+    paths = sorted(glob(join(data_dir, '*%s*' % task, '*tstat%s*' % contrast_num)))
+    imgs = concat_imgs(paths)
+    thresh_imgs = threshold_img(imgs, 2)
+    contrast_percent = mean_img(thresh_imgs)
+    plot_stat_map(contrast_percent, title=contrast_name,
+                  output_file = '../Plots/%s_%s_percent_exceed.png' % (task, contrast_name))
