a9625086522f5809b4038f7e654581a3c9db2665
Ian <ianeisenberg90@gmail.com>
2018-01-12 09:30:14 -0800

    minor change

--- fmri_analysis/repo_utils/inspect_design.py
+++ fmri_analysis/repo_utils/inspect_design.py
@@ -29,11 +29,17 @@ events_df = pd.DataFrame.from_csv(event_path, sep='\t')
 from nilearn.image import concat_imgs, mean_img, threshold_img
 from nilearn.plotting import plot_glass_brain, plot_stat_map
 
-for contrast_num in range(1, len(contrasts)+1):
-    contrast_name = contrasts[contrast_num-1][0]
-    paths = sorted(glob(join(data_dir, '*%s*' % task, '*tstat%s*' % contrast_num)))
-    imgs = concat_imgs(paths)
-    thresh_imgs = threshold_img(imgs, 2)
-    contrast_percent = mean_img(thresh_imgs)
-    plot_stat_map(contrast_percent, title=contrast_name,
-                  output_file = '../Plots/%s_%s_percent_exceed.png' % (task, contrast_name))
+for task in ['ANT', 'stroop']:
+    contrast_paths = sorted(glob(join(data_dir, '*%s*' % task, 'contrasts.pkl')))
+    contrast_path = contrast_paths[subj_index]
+    contrasts = pickle.load(open(contrast_path, 'rb'))
+
+    for contrast_num in range(1, len(contrasts)+1):
+        contrast_name = contrasts[contrast_num-1][0]
+        paths = sorted(glob(join(data_dir, '*%s*' % task, '*tstat%s*' % contrast_num)))
+        imgs = concat_imgs(paths)
+        thresh_imgs = threshold_img(imgs, 2)
+        contrast_percent = mean_img(thresh_imgs)
+        plot_stat_map(contrast_percent, title=contrast_name,
+                      output_file = '../Plots/%s_%s_percent_exceed.png' % (task, contrast_name),
+                      vmax=1)
