81be214f90fc1c5f37436e499675abdd1b5ee57b
Ian <ianeisenberg90@gmail.com>
2018-04-27 23:14:50 -0700

    updates to event files

--- behavioral_data_prep/transfer_events.py
+++ behavioral_data_prep/transfer_events.py
@@ -23,19 +23,23 @@ def move_EV(subj, task, fmri_dir):
     return new_events_file
     
 def move_EVs(fmri_dir, tasks, overwrite=True, verbose=False):
-    file_dir = path.dirname(__file__)
-    events_dir = join(file_dir, '../behavioral_data/event_files')
     created_files = []
     for subj_file in sorted(glob(join(fmri_dir,'sub-s???'))):
         subj = basename(subj_file)
+        if verbose: print('Transferring subject %s' % subj)
         for task in tasks:
-            if overwrite==True or not exists(join(subj_file,'*',
-                                                 'func', '*%s*' % task)):
-                try:
-                    name = move_EV(subj, task, events_dir, fmri_dir)
-                    created_files.append(name)
-                except IndexError:
-                    print('Move_EV failed for the %s: %s' % (subj, task))
+            bold_files = glob(join(subj_file,'*', 'func', '*%s*bold.nii.gz' % task))
+            assert len(bold_files) <= 1, "Multiple bold files found for %s_%s" % (subj, task)
+            if len(bold_files) == 1:
+                event_files = glob(join(subj_file,'*', 'func', '*%s*events.tsv' % task))
+                if overwrite==True or len(event_files)==0:
+                    try:
+                        name = move_EV(subj, task, fmri_dir)
+                        created_files.append(name)
+                    except IndexError:
+                        print('Move_EV failed for the %s: %s' % (subj, task))
+            else:
+                print('**** No %s bold found' % task)
     if verbose:
         print('\n'.join(created_files))
         
@@ -44,11 +48,12 @@ def move_EVs(fmri_dir, tasks, overwrite=True, verbose=False):
 parser = argparse.ArgumentParser(description='Example BIDS App entrypoint script.')
 parser.add_argument('-data_dir', default='/data')
 parser.add_argument('--tasks', default=None)
-parser.add_argument('--overwrite_event')
+parser.add_argument('--overwrite_event', action='store_true')
+parser.add_argument('--verbose', action='store_true')
 args = parser.parse_args()
 
-# list of subject identifi
-ers
+overwrite_event = args.overwrite_event
+verbose = args.verbose
 data_dir = args.data_dir
 if args.tasks:
     task_list = args.tasks
@@ -58,4 +63,4 @@ else:
                'stopSignal', 'stroop', 'surveyMedley',
                'twoByTwo', 'WATT3']
 
-move_EVs(data_dir, task_list, args.overwrite_event)
+move_EVs(data_dir, task_list, overwrite_event, verbose=verbose)
