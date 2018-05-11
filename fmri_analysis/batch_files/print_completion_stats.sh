406a2a73e0f0f848c18e0809a196323c1c584edc
Ian <IanEisenberg@gmail.com>
2018-05-11 12:14:22 -0700

    added review scripts

---
+++ fmri_analysis/batch_files/print_completion_stats.sh
@@ -0,0 +1,10 @@
+derivatives_loc=`sed '6q;d' singularity_config.txt`
+for task in ANT CCTHot discountFix DPX motorSelectiveStop stopSignal stroop surveyMedley twoByTwo WATT3
+do
+    echo "***************************************************************"
+    echo $task
+    rtcount=`ls ${derivatives_loc}/1stLevel/*/$task/*-rt/*/cope1* | wc -l`
+    nortcount=`ls ${derivatives_loc}/1stLevel/*/$task/*-nort/*/cope1* | wc -l`
+    echo "    contrast RT count: $rtcount"
+    echo "    contrast noRT count: $nortcount"
+done
