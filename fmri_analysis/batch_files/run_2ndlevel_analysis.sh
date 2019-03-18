55dc48dba12473e825f8fd2324dad1ddddc27060
Ian Eisenberg <ianeisenberg90@gmail.com>
2019-03-18 10:14:21 -0700

    minor bug fix

--- fmri_analysis/batch_files/run_2ndlevel_analysis.sh
+++ fmri_analysis/batch_files/run_2ndlevel_analysis.sh
@@ -1,5 +1,5 @@
 for task in ANT CCTHot DPX discountFix motorSelectiveStop stopSignal stroop twoByTwo WATT3
 do
-    sed -e "s/{task}/$task/g" -e "s/{rt_flag}/g"  2ndlevel_analysis.batch | sbatch 
+    sed -e "s/{task}/$task/g" 2ndlevel_analysis.batch | sbatch 
 done
 
