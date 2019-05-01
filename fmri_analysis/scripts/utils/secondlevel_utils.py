bb2cbffd1bd9ae30896bf96e5308e7a8ad6d2bbb
Ian Eisenberg <ianeisenberg90@gmail.com>
2019-05-01 07:52:49 -0700

    fixed brain mask bug

--- fmri_analysis/scripts/utils/secondlevel_utils.py
+++ fmri_analysis/scripts/utils/secondlevel_utils.py
@@ -10,7 +10,7 @@ def create_group_mask(fmriprep_dir, threshold=.8, verbose=True):
     if verbose:
         print('Creating Group mask...')
     brainmasks = glob(path.join(fmriprep_dir,'sub-s???',
-                               '*','func','*MNI152NLin2009cAsym_brainmask*'))
+                               '*','func','*MNI152NLin2009cAsym*brain_mask.nii.gz'))
     mean_mask = image.mean_img(brainmasks)
     group_mask = image.math_img("a>=%s" % str(threshold), a=mean_mask)
     return group_mask
