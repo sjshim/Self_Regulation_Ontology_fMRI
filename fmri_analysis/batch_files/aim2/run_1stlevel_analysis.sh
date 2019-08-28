c917235dc403441ba8c8e41993783b7170f21af5
mckenziephagen <mphagen@sh-ln07.stanford.edu>
2019-08-27 21:55:14 -0700

    batch file for uh2 updated

--- fmri_analysis/batch_files/aim2/run_1stlevel_analysis.sh
+++ fmri_analysis/batch_files/aim2/run_1stlevel_analysis.sh
@@ -1,4 +1,4 @@
-for task in discountFix motorSelectiveStop stopSignal  
+for task in manipulationTask motorSelectiveStop stopSignal discountFix 
 do
     sed -e "s/{task}/$task/g" -e "s/{rt_flag}/--rt/g"  1stlevel_analysis.batch | sbatch 
     sed -e "s/{task}/$task/g" -e "s/{rt_flag}//g"  1stlevel_analysis.batch | sbatch
