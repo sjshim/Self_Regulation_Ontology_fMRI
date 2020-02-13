ee341857618ef943aa31b44a6aed8318d18a0004
henrymj <henrymj@stanford.edu>
2020-02-13 10:18:36 -0800

    update 2ndlevel mask paths

--- fmri_analysis/scripts/utils/secondlevel_utils.py
+++ fmri_analysis/scripts/utils/secondlevel_utils.py
@@ -10,8 +10,14 @@ from utils.utils import get_flags
 def create_group_mask(fmriprep_dir, threshold=.8, verbose=True):
     if verbose:
         print('Creating Group mask...')
-    brainmasks = glob(path.join(fmriprep_dir,'sub-*',
-                               'func','*MNI152NLin2009cAsym*brain_mask.nii.gz'))     
+    #check if there's a session folder 
+    if len(glob(path.join(fmriprep_dir,'sub-*','func','*MNI152NLin2009cAsym*brain_mask.nii.gz'))):
+        brainmasks = glob(path.join(fmriprep_dir,'sub-*',
+                            'func','*MNI152NLin2009cAsym*brain_mask.nii.gz'))
+    else:
+        brainmasks = glob(path.join(fmriprep_dir,'sub-*', '*',
+                            'func','*MNI152NLin2009cAsym*brain_mask.nii.gz'))
+                      
     mean_mask = image.mean_img(brainmasks)
     group_mask = image.math_img("a>=%s" % str(threshold), a=mean_mask)
     return group_mask
