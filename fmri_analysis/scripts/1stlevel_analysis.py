18e90c519ba1d2992cd4d6dd6bc5e3a6c40191e9
henrymj <henrymj@stanford.edu>
2020-09-01 14:36:17 -0700

    flake analysis scripts

--- fmri_analysis/scripts/1stlevel_analysis.py
+++ fmri_analysis/scripts/1stlevel_analysis.py
@@ -7,27 +7,15 @@
 
 
 import argparse
-from inspect import currentframe, getframeinfo
 from glob import glob
-import numpy as np
 import os
 from os.path import join
-import pandas as pd
-from pathlib import Path
-import pickle
 import sys
-import nibabel as nib
 from nistats.first_level_model import FirstLevelModel
 import warnings
-from utils.firstlevel_plot_utils import plot_design
-from utils.firstlevel_utils import get_first_level_objs, make_first_level_obj, save_first_level_obj
+from utils.firstlevel_utils import get_first_level_objs,\
+    make_first_level_obj, save_first_level_obj
 
-
-# ### Parse Arguments
-# These are not needed for the jupyter notebook, but are used after conversion to a script for production
-# 
-# - conversion command:
-#   - jupyter nbconvert --to script --execute 1stlevel_analysis.ipynb
 
 # In[2]:
 
@@ -38,37 +26,33 @@ parser.add_argument('-derivatives_dir', default=None)
 parser.add_argument('-fmriprep_dir', default=None)
 parser.add_argument('-working_dir', default=None)
 parser.add_argument('--subject_ids', nargs="+")
-parser.add_argument('--tasks', nargs="+", help="Choose from ANT, CCTHot, discountFix,                                     DPX, motorSelectiveStop, stopSignal,                                     stroop, surveyMedley, twoByTwo, WATT3")
+parser.add_argument('--tasks', nargs="+", help="Choose from ANT, CCTHot, discountFix, DPX, motorSelectiveStop, stopSignal, stroop, surveyMedley, twoByTwo, WATT3")
 parser.add_argument('--rt', action='store_true')
 parser.add_argument('--beta', action='store_true')
 parser.add_argument('--n_procs', default=16, type=int)
 parser.add_argument('--overwrite', action='store_true')
 parser.add_argument('--quiet', '-q', action='store_true')
-parser.add_argument('--design_matrix', '-dm', action='store_true')
+parser.add_argument('--design_matrix_only', '-dm', action='store_true')
 parser.add_argument('--a_comp_cor', action='store_true')
 
 if '-derivatives_dir' in sys.argv or '-h' in sys.argv:
     args = parser.parse_args()
 else:
     args = parser.parse_args([])
-    args.tasks = ['discountFix', 'manipulationTask', #aim 2 tasks
+    args.tasks = ['discountFix', 'manipulationTask',  # aim 2 tasks
                   'motorSelectiveStop', 'stopSignal']
-    
     # args.tasks = ['ANT', 'CCTHot', 'discountFix', 'DPX', #aim 1 tasks
-    #               'motorSelectiveStop', 'stopSignal', 
+    #               'motorSelectiveStop', 'stopSignal',
     #               'stroop', 'twoByTwo', 'WATT3']
-    
-   
-    #args.subject_ids = ['3010']
 
-    args.rt=True
-    args.a_comp_cor=True
+    args.rt = True
+    args.a_comp_cor = True
 
-    args.n_procs=1
+    args.n_procs = 1
     args.derivatives_dir = '/data/derivatives/'
     args.data_dir = '/data'
     args.fmriprep_dir = '/data/derivatives/fmriprep/'
-    args.design_matrix=True #False
+    args.design_matrix_only = False
 
 
 # In[3]:
@@ -78,24 +62,20 @@ if not args.quiet:
     def verboseprint(*args, **kwargs):
         print(*args, **kwargs)
 else:
-    verboseprint = lambda *a, **k: None # do-nothing function
+    def verboseprint(*args, **kwards):  # do-nothing function
+        pass
 
-
-# ### Initial Setup
-# 
-# Organize paths and set parameters based on arguments
 
 # In[4]:
-
 
 # Set Paths
 derivatives_dir = args.derivatives_dir
 if args.fmriprep_dir is None:
-    fmriprep_dir = join(derivatives_dir, 'fmriprep', 'fmriprep')
+    fmriprep_dir = join(derivatives_dir, 'fmriprep')
 else:
     fmriprep_dir = args.fmriprep_dir
 data_dir = args.data_dir
-first_level_dir = join(derivatives_dir,'1stlevel')
+first_level_dir = join(derivatives_dir, '1stlevel')
 if args.working_dir is None:
     working_dir = join(derivatives_dir, '1stlevel_workingdir')
 else:
@@ -105,86 +85,73 @@ else:
 if args.tasks is not None:
     tasks = args.tasks
 else:
-    tasks = ['discountFix', 'manipulationTask', 
-                  'motorSelectiveStop', 'stopSignal']
-    
-    # tasks = ['ANT', 'CCTHot', 'discountFix', 'DPX', 
-    #               'motorSelectiveStop', 'stopSignal', 
+    tasks = ['discountFix', 'manipulationTask',
+             'motorSelectiveStop', 'stopSignal']
+
+    # tasks = ['ANT', 'CCTHot', 'discountFix', 'DPX',
+    #               'motorSelectiveStop', 'stopSignal',
     #               'stroop', 'twoByTwo', 'WATT3']
-    
+
 
 # list of subject identifiers
 if not args.subject_ids:
-    subjects = sorted([i.split("-")[-1] for i in glob(os.path.join(args.data_dir, '*')) if 'sub-' in i])
+    subjects = sorted([i.split("-")[-1] for i in glob(
+                       os.path.join(args.data_dir, '*')) if 'sub-' in i])
 else:
     subjects = args.subject_ids
-    
+
 # other arguments
 regress_rt = args.rt
 beta_series = args.beta
 n_procs = args.n_procs
 # TR of functional images
 TR = .68
-a_comp_cor=args.a_comp_cor
+a_comp_cor = args.a_comp_cor
 
 # In[5]:
 
-
-#print
 verboseprint('*'*79)
-verboseprint('Tasks: %s\n, Subjects: %s\n, derivatives_dir: %s\n, data_dir: %s' % 
-     (tasks, subjects, derivatives_dir, data_dir))
+verboseprint(
+    'Tasks: %s\n, Subjects: %s\n, derivatives_dir: %s\n, data_dir: %s' %
+    (tasks, subjects, derivatives_dir, data_dir))
 verboseprint('*'*79)
-
-
-# # Set up Nodes
-
-# ### Run analysis
-# 
-# gather the files for each task within each subject
-# 
 
 # In[6]:
-
 
 to_run = []
 for subject_id in subjects:
     for task in tasks:
         verboseprint('Setting up %s, %s' % (subject_id, task))
-        files = get_first_level_objs(subject_id, task, first_level_dir, regress_rt=regress_rt)
+        files = get_first_level_objs(
+            subject_id, task, first_level_dir, regress_rt=regress_rt)
         if len(files) == 0 or args.overwrite:
             with warnings.catch_warnings():
-                warnings.filterwarnings("ignore",category=DeprecationWarning)
-                warnings.filterwarnings("ignore",category=UserWarning)
-                subjinfo = make_first_level_obj(subject_id, task, fmriprep_dir, 
-                                                data_dir, first_level_dir, TR, regress_rt=regress_rt, a_comp_cor=a_comp_cor)
+                warnings.filterwarnings("ignore", category=DeprecationWarning)
+                warnings.filterwarnings("ignore", category=UserWarning)
+                subjinfo = make_first_level_obj(subject_id, task, fmriprep_dir,
+                                                data_dir, first_level_dir, TR,
+                                                regress_rt=regress_rt,
+                                                a_comp_cor=a_comp_cor)
             if subjinfo is not None:
                 to_run.append(subjinfo)
 
-
-# ### Run model fit
-# 
-# generate the glm and fit the timeseries data to it
-
 # In[7]:
-
 
 for subjinfo in to_run:
     verboseprint(subjinfo.ID)
     verboseprint('** fitting model')
-    fmri_glm = FirstLevelModel(TR, 
-                           subject_label = subjinfo.ID,
-                           mask=subjinfo.mask,
-                           noise_model='ar1',
-                           standardize=False, 
-                           hrf_model='spm',
-                           drift_model='cosine',
-                           period_cut=80,
-                           n_jobs=1
-                          )
-    
-    print(args.design_matrix)
-    if args.design_matrix:
+    fmri_glm = FirstLevelModel(TR,
+                               subject_label=subjinfo.ID,
+                               mask=subjinfo.mask,
+                               noise_model='ar1',
+                               standardize=False,
+                               hrf_model='spm',
+                               drift_model='cosine',
+                               period_cut=80,
+                               n_jobs=1
+                               )
+
+    if args.design_matrix_only:
         verboseprint('** saving')
         save_first_level_obj(subjinfo, first_level_dir, False)
         subjinfo.export_design(first_level_dir)
@@ -197,10 +164,3 @@ for subjinfo in to_run:
         save_first_level_obj(subjinfo, first_level_dir, True)
         subjinfo.export_design(first_level_dir)
         subjinfo.export_events(first_level_dir)
-
-
-# In[ ]:
-
-
-
-
--- fmri_analysis/scripts/2ndlevel_analysis.py
+++ fmri_analysis/scripts/2ndlevel_analysis.py
@@ -2,42 +2,27 @@
 # coding: utf-8
 
 # In[ ]:
-
-
 import argparse
-from glob import glob
 from os import makedirs, path
 import pandas as pd
-import pickle
 import sys
 
 from nistats.second_level_model import SecondLevelModel
-from nistats.thresholding import map_threshold
-from nilearn import plotting
-from utils.firstlevel_utils import (get_first_level_objs, 
-                                    get_first_level_maps, 
-                                    load_first_level_objs, 
-                                    FirstLevel)
+from utils.firstlevel_utils import get_first_level_maps
 from utils.secondlevel_utils import create_group_mask, randomise
 from utils.utils import get_contrasts, get_flags
 
-
-# ### Parse Arguments
-# These are not needed for the jupyter notebook, but are used after conversion to a script for production
-# 
-# - conversion command:
-#   - jupyter nbconvert --to script --execute 2ndlevel_analysis.ipynb
-
 # In[ ]:
-
 
 parser = argparse.ArgumentParser(description='2nd level Entrypoint Script.')
 parser.add_argument('-derivatives_dir', default=None)
-parser.add_argument('--tasks', nargs="+", help="Choose from ANT, CCTHot, discountFix,                                     DPX, motorSelectiveStop, stopSignal,                                     stroop, surveyMedley, twoByTwo, WATT3")
+parser.add_argument('--tasks', nargs="+", help="Choose from ANT, CCTHot, discountFix, DPX, motorSelectiveStop, stopSignal, stroop, surveyMedley, twoByTwo, WATT3")
 parser.add_argument('--rerun', action='store_true')
 parser.add_argument('--rt', action='store_true')
 parser.add_argument('--beta', action='store_true')
 parser.add_argument('--n_perms', default=1000, type=int)
+parser.add_argument('--mask_thresh', default=.95, type=float)
+parser.add_argument('--smoothing_fwhm', default=6)
 parser.add_argument('--quiet', '-q', action='store_true')
 parser.add_argument('--aim', default='NONE', help='Choose from aim1, aim2')
 parser.add_argument('--group', default='NONE')
@@ -48,9 +33,8 @@ else:
     args = parser.parse_args([])
     args.derivatives_dir = '/data/derivatives/'
     args.tasks = ['stroop']
-    args.rt=True
+    args.rt = True
     args.n_perms = 10
-
 
 # In[ ]:
 
@@ -59,19 +43,14 @@ if not args.quiet:
     def verboseprint(*args, **kwargs):
         print(*args, **kwargs)
 else:
-    verboseprint = lambda *a, **k: None # do-nothing function
-
-
-# ### Setup
-# 
-# Organize paths and set parameters based on arguments
+    def verboseprint(*args, **kwards):  # do-nothing function
+        pass
 
 # In[ ]:
 
-
 # set paths
 first_level_dir = path.join(args.derivatives_dir, '1stlevel')
-second_level_dir = path.join(args.derivatives_dir,'2ndlevel')
+second_level_dir = path.join(args.derivatives_dir, '2ndlevel')
 fmriprep_dir = path.join(args.derivatives_dir, 'fmriprep')
 
 # set tasks
@@ -79,85 +58,106 @@ if args.tasks is not None:
     tasks = args.tasks
 else:
     tasks = ['ANT', 'CCTHot', 'discountFix',
-            'DPX', 'motorSelectiveStop',
-            'stopSignal', 'stroop',
-            'twoByTwo', 'WATT3']
-    
+             'DPX', 'motorSelectiveStop',
+             'stopSignal', 'stroop',
+             'twoByTwo', 'WATT3']
+
 # set other variables
 regress_rt = args.rt
 beta_series = args.beta
 n_perms = args.n_perms
 group = args.group
 
-# ### Create Mask
-
 # In[ ]:
 
-
-mask_threshold = .95
-mask_loc = path.join(second_level_dir, 'group_mask_thresh-%s.nii.gz' % str(mask_threshold))
-if path.exists(mask_loc) == False or args.rerun:
+# Create Mask
+mask_loc = path.join(second_level_dir,
+                     'group_mask_thresh-%s.nii.gz' % str(args.mask_thresh))
+if (not path.exists(mask_loc)) or args.rerun:
     verboseprint('Making group mask')
-    group_mask = create_group_mask(fmriprep_dir, mask_threshold)
+    group_mask = create_group_mask(fmriprep_dir, args.mask_thresh)
     makedirs(path.dirname(mask_loc), exist_ok=True)
     group_mask.to_filename(mask_loc)
 
-
-# ### Create second level objects
-# Gather first level models and create second level model
-
 # In[ ]:
 
-# Set up 2ndlevel Regressors - Added by HMJ 7.29.20
-aim1_2ndlevel_confounds_path = "/scripts/aim1_2ndlevel_regressors/aim1_2ndlevel_confounds_matrix.csv"
-full_confounds_df = pd.read_csv(aim1_2ndlevel_confounds_path, index_col='index')
 
-def get_2ndlevel_designMatrix_andContrasts(maps, task):
+aim1_2ndlevel_confounds_path = "/scripts/aim1_2ndlevel_regressors/aim1_2ndlevel_confounds_matrix.csv"
+full_confounds_df = pd.read_csv(aim1_2ndlevel_confounds_path,
+                                index_col='index')
+
+
+def fit_and_compute_contrast(maps, task, second_level_model):
+    design_matrix, curr_contrasts = get_group_DM_and_contrasts(maps,
+                                                               task)
+    maps, design_matrix = filter_maps_and_DM(maps, design_matrix)
+    second_level_model.fit(maps,
+                           design_matrix=design_matrix)
+    contrast_map = second_level_model.compute_contrast(
+        second_level_contrast=curr_contrasts)
+    return contrast_map, maps
+
+
+def get_group_DM_and_contrasts(maps, task):
     design_matrix = pd.DataFrame([1] * len(maps), columns=['intercept'])
     contrasts = [1]
-    if args.aim=='aim1':
+    if args.aim == 'aim1':
         subjects = [m.split('1stlevel/')[-1].split('/')[0] for m in maps]
-        design_matrix = full_confounds_df.loc[subjects, ['age', 'sex', task+'_meanFD']].copy()
+        design_matrix = full_confounds_df.loc[subjects,
+                                              ['age', 'sex', task+'_meanFD']
+                                              ].copy()
         design_matrix.index.rename('subject_label', inplace=True)
         design_matrix['intercept'] = 1
         contrasts = [0, 0, 0, 1]
     return design_matrix, contrasts
 
-def filter_maps_designMatrix(maps, design_matrix):
+
+def filter_maps_and_DM(maps, design_matrix):
     drop_num = design_matrix.isna().any(axis=1).sum()
-    print('dropping ' + str(drop_num) +' due to missing values in design matrix')
+    print('dropping ' +
+          str(drop_num) +
+          ' due to missing values in design matrix')
     design_matrix = design_matrix.dropna()
-    if len(design_matrix)!=len(maps):
-        keep_subs = dm.index.tolist()
-        maps = [m for m in maps if m.split('1stlevel/')[-1].split('/')[0] in keep_subs]
-    assert(len(design_matrix)==len(maps))
+    if len(design_matrix) != len(maps):
+        keep_subs = design_matrix.index.tolist()
+        maps = [m for m in maps if
+                m.split('1stlevel/')[-1].split('/')[0] in keep_subs]
+    assert(len(design_matrix) == len(maps))
     return maps, design_matrix
 
 
 rt_flag, beta_flag = get_flags(regress_rt, beta_series)
 for task in tasks:
     verboseprint('Running 2nd level for %s' % task)
-    # load first level models
-    # create contrast maps
+
     verboseprint('*** Creating maps')
     task_contrasts = get_contrasts(task, regress_rt)
-    maps_dir = path.join(second_level_dir, task, 'secondlevel-%s_%s_maps' % (rt_flag, beta_flag))
+    maps_dir = path.join(second_level_dir,
+                         task,
+                         'secondlevel-%s_%s_maps' % (rt_flag, beta_flag))
     makedirs(maps_dir, exist_ok=True)
+
     # run through each contrast for all participants
     if group == 'NONE':
         for name, contrast in task_contrasts:
-            second_level_model = SecondLevelModel(mask=mask_loc, smoothing_fwhm=6)
-            maps = get_first_level_maps('*', task, first_level_dir, name, regress_rt, beta_series)
+            second_level_model = SecondLevelModel(
+                mask=mask_loc,
+                smoothing_fwhm=args.smoothing_fwhm
+                )
+            maps = get_first_level_maps('*', task,
+                                        first_level_dir,
+                                        name,
+                                        regress_rt,
+                                        beta_series)
             N = str(len(maps)).zfill(2)
             verboseprint('****** %s, %s files found' % (name, N))
             if len(maps) <= 1:
                 verboseprint('****** No Maps')
                 continue
-            # CHANGED TO HANDLE GROUP COVARIATES
-            design_matrix, curr_contrasts = get_2ndlevel_designMatrix_andContrasts(maps, task)
-            maps, design_matrix = filter_maps_designMatrix(maps, design_matrix)
-            second_level_model.fit(maps, design_matrix=design_matrix)
-            contrast_map = second_level_model.compute_contrast(second_level_contrast=curr_contrasts)
+
+            contrast_map, maps = fit_and_compute_contrast(maps,
+                                                          task,
+                                                          second_level_model)
             # save
             contrast_file = path.join(maps_dir, 'contrast-%s.nii.gz' % name)
             contrast_map.to_filename(contrast_file)
@@ -170,85 +170,57 @@ for task in tasks:
                 randomise(maps, maps_dir, mask_loc, n_perms=n_perms)
                 # write metadata
                 with open(path.join(maps_dir, 'metadata.txt'), 'a') as f:
-                    f.write('Contrast-%s: Randomise run with %s permutations\n' % (contrast, str(n_perms)))
-    # run through each contrast within each group
+                    f.write(
+                        'Contrast-%s: Randomise run with %s permutations\n' %
+                        (contrast, str(n_perms)))
+
     else:
         verboseprint('*** Creating %s maps' % group)
-        f = open("/scripts/%s_subjects.txt" % group,"r") 
+        f = open("/scripts/%s_subjects.txt" % group, "r")
         group_subjects = f.read().split('\n')
         for name, contrast in task_contrasts:
-            second_level_model = SecondLevelModel(mask=mask_loc, smoothing_fwhm=6)
+            second_level_model = SecondLevelModel(
+                mask=mask_loc,
+                smoothing_fwhm=args.smoothing_fwhm
+                )
             maps = []
             for curr_subject in group_subjects:
-                curr_map = get_first_level_maps(curr_subject, task, first_level_dir, name, regress_rt, beta_series)
-                if len(curr_map): #if a map is present
-                    # maps.append(curr_map[0])
+                curr_map = get_first_level_maps(curr_subject, task,
+                                                first_level_dir,
+                                                name,
+                                                regress_rt,
+                                                beta_series)
+                if len(curr_map):
                     maps += curr_map
             N = str(len(maps)).zfill(2)
             verboseprint('****** %s, %s files found' % (name, N))
             if len(maps) <= 1:
                 verboseprint('****** No Maps')
                 continue
-            # CHANGED TO HANDLE GROUP COVARIATES
-            design_matrix, curr_contrasts = get_2ndlevel_designMatrix_andContrasts(maps, task)
-            maps, design_matrix = filter_maps_designMatrix(maps, design_matrix)
-            second_level_model.fit(maps, design_matrix=design_matrix)
-            contrast_map = second_level_model.compute_contrast(second_level_contrast=curr_contrasts)
+            contrast_map, maps = fit_and_compute_contrast(maps,
+                                                          task,
+                                                          second_level_model)
             # save
-            makedirs(path.join(maps_dir, group), exist_ok=True)
-            contrast_file = path.join(maps_dir, group, 'contrast-%s-%s.nii.gz' % (name, group))
+            group_dir = path.join(maps_dir, group)
+            makedirs(group_dir, exist_ok=True)
+            contrast_file = path.join(group_dir,
+                                      'contrast-%s-%s.nii.gz' % (name, group))
             contrast_map.to_filename(contrast_file)
             # write metadata
-            with open(path.join(maps_dir, group, 'metadata.txt'), 'a') as f:
+            with open(path.join(group_dir, 'metadata.txt'), 'a') as f:
                 f.write('Contrast-%s-%s: %s maps\n' % (contrast, group, N))
             # save corrected map
             if n_perms > 0:
                 verboseprint('*** Running Randomise')
-                randomise(maps, path.join(maps_dir, group), mask_loc, n_perms=n_perms, group=group)
+                randomise(maps, group_dir,
+                          mask_loc,
+                          n_perms=n_perms,
+                          group=group)
                 # write metadata
-                with open(path.join(maps_dir, group, 'metadata.txt'), 'a') as f:
-                    f.write('Contrast-%s-%s: Randomise run with %s permutations\n' % (contrast, group, str(n_perms)))
-
+                with open(path.join(group_dir, 'metadata.txt'), 'a') as f:
+                    f.write(
+                        'Contrast-%s-%s: Randomise run with %s permutations\n'
+                        %
+                        (contrast, group, str(n_perms)))
 
     verboseprint('Done with %s' % task)
-
-
-# In[ ]:
-
-
-"""
-# Using nistats method of first level objects. Not conducive for randomise.
-rt_flag, beta_flag = get_flags(regress_rt, beta_series)
-for task in tasks:
-    verboseprint('Running 2nd level for %s' % task)
-    # load first level models
-    first_levels = load_first_level_objs(task, first_level_dir, regress_rt=regress_rt)
-    if len(first_levels) == 0:
-        continue
-    first_level_models = [subj.fit_model for subj in first_levels]
-    N = str(len(first_level_models)).zfill(2)
-
-    # simple design for one sample test
-    design_matrix = pd.DataFrame([1] * len(first_level_models), columns=['intercept'])
-    
-    # run second level
-    verboseprint('*** Running model. %s first level files found' % N)
-    second_level_model = SecondLevelModel(mask=mask_loc, smoothing_fwhm=6).fit(
-        first_level_models, design_matrix=design_matrix)
-    makedirs(path.join(second_level_dir, task), exist_ok=True)
-    f = open(path.join(second_level_dir, task, 'secondlevel_%s_%s.pkl' % (rt_flag, beta_flag)), 'wb')
-    pickle.dump(second_level_model, f)
-    f.close()
-    
-    # create contrast maps
-    verboseprint('*** Creating maps')
-    task_contrasts = get_contrasts(task, regress_rt)
-    maps_dir = path.join(second_level_dir, task, 'secondlevel_%s_%s_N-%s_maps' % (rt_flag, beta_flag, N))
-    makedirs(maps_dir, exist_ok=True)
-    for name, contrast in task_contrasts:
-        verboseprint('****** %s' % name)
-        contrast_map = second_level_model.compute_contrast(first_level_contrast=contrast)
-        contrast_file = path.join(maps_dir, 'contrast-%s.nii.gz' % name)
-        contrast_map.to_filename(contrast_file)
-"""
-
